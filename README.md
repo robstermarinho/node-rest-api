9ec1106055b1249d5dbadcefd829ccb7e9c74414
Robert Marinho <robstermarinho@gmail.com>
2019-06-30 13:41:59 -0300

    Create the other endpoints

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 # REST API with Node and Express
 
+Simple REST API to store projects and tasks.
+
 To start the server at `http://localhost:3000` run the following command:
 
 ```
 yarn runserver
 ```
+
+## Routes
+
+- `POST - /projects/`
+
+  - Add a new project
+  - Inputs: `id`, `title`
+  - Output: `{ id: "1", title: 'Project Title', tasks: [] }`
+
+- `GET - /projects/`
+
+  - List all the projects and tasks
+
+- `PUT /projects/:id/`:
+
+  - Update the title of the project `id`
+
+- `DELETE /projects/:id/`:
+
+  - Delete the project `id`
+
+- `POST /projects/:id/tasks/`:
+  - Stores a new task title in the project `id`
+  - Inputs: `title`
--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 const express = require("express");
 const server = express();
-
-// Initial Data
 const PROJECTS = [];
+const CONFIG = {
+  number_of_requests: 0
+};
 
-const MyMiddleware = (req, res, next) => {
-  console.log("123");
-  next();
+// Custom Middleware to check if the project exists
+const CheckProjectIDExists = (req, res, next) => {
+  const { id } = req.params;
+  if (!PROJECTS[id]) {
+    return res.status(400).json({
+      error: "Project id does not exist."
+    });
+  }
+  return next();
+};
+
+const CheckNewProjectExists = (req, res, next) => {
+  const { id } = req.body;
+  if (PROJECTS[id]) {
+    return res.status(400).json({
+      error: `Project id ${id} already exists.`
+    });
+  }
+  return next();
 };
 
 // Built-in middleware to parse incoming requests with JSON payloads.
 server.use(express.json());
-// Custom middlewares
-server.use((req, res, next) => MyMiddleware(req, res, next));
+// Custom Global Middleware
+server.use((request, response, next) => {
+  CONFIG.number_of_requests += 1;
+  //Log the requessts
+  console.log(
+    `REQUEST ${CONFIG.number_of_requests}: ${request.method} - ${request.url}`
+  );
+  //Continue the next request
+  next();
+});
+
+// GET - Listing all projects
+server.get("/projects/", (req, res) => {
+  return res.json(PROJECTS);
+});
 
 //POST - Create a Project
-server.post("/projects/", (req, res) => {
+server.post("/projects/", CheckNewProjectExists, (req, res) => {
   // Get post parameters
   const { id, title } = req.body;
 
@@ -25,10 +55,30 @@ server.post("/projects/", (req, res) => {
     title: title,
     tasks: []
   };
-  // Add new project
-  PROJECTS.push(project);
 
+  PROJECTS[id] = project;
   return res.json(project);
+});
+
+// PUT - Update Title - Change project title
+server.put("/projects/:id/", CheckProjectIDExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  PROJECTS[id].title = title;
+  return res.json(PROJECTS[id]);
+});
+
+server.delete("/projects/:id/", CheckProjectIDExists, (req, res) => {
+  const { id } = req.params;
+  PROJECTS.splice(id, 1);
+  return res.json(PROJECTS);
+});
+
+server.post("/projects/:id/tasks/", CheckProjectIDExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  PROJECTS[id].tasks.push(title);
+  return res.json(PROJECTS[id]);
 });
 
 // Listen to the port 3000
