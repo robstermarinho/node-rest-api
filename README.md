47744461f6e12eb72fe6fca8fca1acc8d694cf1b
Robert Marinho <robstermarinho@gmail.com>
2019-06-30 02:23:14 -0300

    Create Projects

---
+++ README.md
@@ -0,0 +1,7 @@
+# REST API with Node and Express
+
+To start the server at `http://localhost:3000` run the following command:
+
+```
+yarn runserver
+```
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello World!!!");
+const express = require("express");
+const server = express();
+
+// Initial Data
+const PROJECTS = [];
+
+const MyMiddleware = (req, res, next) => {
+  console.log("123");
+  next();
+};
+
+// Built-in middleware to parse incoming requests with JSON payloads.
+server.use(express.json());
+// Custom middlewares
+server.use((req, res, next) => MyMiddleware(req, res, next));
+
+//POST - Create a Project
+server.post("/projects/", (req, res) => {
+  // Get post parameters
+  const { id, title } = req.body;
+
+  // Set project
+  const project = {
+    id: id,
+    title: title,
+    tasks: []
+  };
+  // Add new project
+  PROJECTS.push(project);
+
+  return res.json(project);
+});
+
+// Listen to the port 3000
+server.listen(3000);
